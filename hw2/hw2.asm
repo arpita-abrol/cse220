3f5665e1b5aa2acb7b97350d6e3b67321b5af0c4
Arpita <arpita-abrol@users.noreply.github.com>
2018-10-10 01:39:44 -0400

    Part II complete

--- hw2/hw2.asm
+++ hw2/hw2.asm
@@ -18,7 +18,9 @@
 # $sp 		= the stack
 ########	should preserve changed $s or $ra registers that are changed
 
+#####################################################################
 ### Part I ###
+# int index_of_car(car[] cars, int length, int start_index, int year)
 index_of_car:
 	# preserve registers...
 	addi $sp, $sp, -4
@@ -64,15 +66,84 @@ return_index_of_car:
 	addi $sp, $sp, 4
 	jr $ra # return to caller function
 	
-
+#####################################################################
 ### Part II ###
+# int strcmp(string str1, string str2)
 strcmp:
-	li $v0, -200
-	li $v1, -200
+	# preserve registers...
+	addi $sp, $sp, -20
+	sw $ra, 0($sp)
+	sw $s0, 4($sp)
+	sw $s1, 8($sp)
+	sw $s2, 12($sp)
+	sw $s3, 16($sp)
+	
+	# preserve strings
+	move $s0, $a0	# $s0 = str1
+	move $s1, $a1	# $s1 = str2
+	
+	# get string lengths
+	jal get_string_length
+	move $s2, $v0	# $t0 = str1.length()
+	move $a0, $s1
+	jal get_string_length
+	move $s3, $v0	# $t1 = str2.length()
+	
+	beqz $s2, str_one_zero
+	beqz $s3, str_two_zero
+	
+	li $v0, 0 	# default -- for case when str1 = str2
+	li $t2, 0	# ctr
+	
+strcmp_check:
+	lbu $t0, 0($s0)
+	lbu $t1, 0($s1)
+	bne $t0, $t1, strcmp_not_equal
+	beqz $t0, strcmp_end	# if both are equal then if one is null, both are so end
+	addi $t2, $t2, 1
+	addi $s0, $s0, 1
+	addi $s1, $s1, 1
+	j strcmp_check
+	
+strcmp_not_equal:
+	sub $v0, $t0, $t1
+	j strcmp_end
 
+str_one_zero:
+	li $t0, 0
+	sub $v0, $t0, $s3
+	j strcmp_end
+	
+str_two_zero:
+	move $v0, $s2
+	j strcmp_end
+	
+strcmp_end:
+	# restore registers	
+	lw $s3, 16($sp)
+	lw $s2, 12($sp)
+	lw $s1, 8($sp)
+	lw $s0, 4($sp)
+	lw $ra, 0($sp)
+	addi $sp, $sp, 12
 	jr $ra
 
+# int get_string_length(string str)
+get_string_length:
+	li $t0, 0	# ctr
 
+get_string_length_loop:
+	lbu $t1, 0($a0)
+	beqz $t1, get_string_length_done
+	addi $t0, $t0, 1
+	addi $a0, $a0, 1
+	j get_string_length_loop
+	
+get_string_length_done:
+	move $v0, $t0
+	jr $ra
+
+#####################################################################
 ### Part III ###
 memcpy:
 	li $v0, -200
--- hw2/strcmp_main.asm
+++ hw2/strcmp_main.asm
@@ -3,8 +3,8 @@
 .data
 nl: .asciiz "\n"
 strcmp_output: .asciiz "strcmp output: "
-str1: .asciiz "ABCD"
-str2: .asciiz "ABA"
+str1: .asciiz "StonyB"
+str2: .asciiz "Stony"
 
 .text
 .globl main
@@ -12,14 +12,22 @@ main:
 la $a0, strcmp_output
 li $v0, 4
 syscall
+
+# load args, call function
 la $a0, str1
 la $a1, str2
 jal strcmp
+
+# print function call result
 move $a0, $v0
 li $v0, 1
 syscall
+
+# print newline
 la $a0, nl
 li $v0, 4
 syscall
+
+# exit
 li $v0, 10
 syscall
