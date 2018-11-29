62120f91a1dadbf1fbe5b7dc236d271dbba94bb2
Arpita <arpita-abrol@users.noreply.github.com>
2018-11-29 00:18:28 -0500

    Part I complete

---
+++ hw4/hw4.asm
@@ -0,0 +1,313 @@
+# Arpita Abrol
+# aabrol
+# 111504563
+
+#####################################################################
+############### DO NOT CREATE A .data SECTION! ######################
+############### DO NOT CREATE A .data SECTION! ######################
+############### DO NOT CREATE A .data SECTION! ######################
+##### ANY LINES BEGINNING .data WILL BE DELETED DURING GRADING! #####
+#####################################################################
+
+.text
+
+#####################################################################
+# Part I
+# int init_game(string map_filename, Map *map_ptr, Player *player_ptr)
+init_game:
+	move $t0, $a0
+	move $t1, $a1
+	move $t2, $a2
+	
+	# allocate memory on heap
+	li $a0, 1	# buffer size = 1 character
+	li $v0, 9
+	syscall
+	move $t6, $v0
+	
+	# open a file
+	li $v0, 13
+	move $a0, $t0
+	li $a1, 0
+	li $a2, 0
+	syscall
+	move $t4, $v0
+	bltz $v0, init_game_exit
+	
+	# read a file... get NUM_ROWS---------------------------------
+	li $t3, 0	# $t3 = value
+	
+	li $v0, 14
+	move $a0, $t4	# curr location
+	move $a1, $t6	# input buffer
+	li $a2, 1
+	syscall
+	
+	lbu $t8, 0($t6)
+	addi $t8, $t8, -48
+	
+	li $t7, 10
+	mult $t8, $t7
+	mflo $t3
+	
+	li $v0, 14
+	move $a0, $t4	# curr location
+	move $a1, $t6	# input buffer
+	li $a2, 1
+	syscall
+	
+	lbu $t8, 0($t6)
+	addi $t8, $t8, -48
+	
+	add $t3, $t3, $t8	# NUM_ROWS
+	
+	# add to byte 0 of map
+	sb $t3, 0($t1)
+	
+	
+	# read a file... get NUM_COLS---------------------------------
+	###### skip next char... new line
+	li $v0, 14
+	move $a0, $t4	# curr location
+	move $a1, $t6	# input buffer
+	li $a2, 1
+	syscall
+	###### end skip
+	
+	li $t8, 0	# $t3 = value
+	
+	li $v0, 14
+	move $a0, $t4	# curr location
+	move $a1, $t6	# input buffer
+	li $a2, 1
+	syscall
+	
+	lbu $t8, 0($t6)
+	addi $t8, $t8, -48
+	
+	li $t7, 10
+	mult $t8, $t7
+	mflo $t9
+	
+	li $v0, 14
+	move $a0, $t4	# curr location
+	move $a1, $t6	# input buffer
+	li $a2, 1
+	syscall
+	
+	lbu $t8, 0($t6)
+	addi $t8, $t8, -48
+	
+	add $t8, $t9, $t8	# NUM_COLS
+	
+	# add to byte 1 of map
+	addi $t1, $t1, 1
+	sb $t8, 0($t1)
+	addi $t1, $t1, -1
+	
+	###### skip next char... new line
+	li $v0, 14
+	move $a0, $t4	# curr location
+	move $a1, $t6	# input buffer
+	li $a2, 1
+	syscall
+	###### end skip
+	
+	# read a file... get MAP_DATA---------------------------------
+	move $t0, $t1
+	addi $t0, $t0, 2
+	mult $t3, $t8
+	mflo $t9	# max
+	#li $t9, 0	# ctr
+	li $t7, 0	# ctr--new line
+	init_game_loop:
+		bne $t7, $t8, init_game_loop_cont
+		###### skip next char... new line
+		li $v0, 14
+		move $a0, $t4	# curr location
+		move $a1, $t6	# input buffer
+		li $a2, 1
+		syscall
+		###### end skip
+		li $t7, 0	# reset
+		
+		init_game_loop_cont:
+		beqz $t9, init_game_loop_end
+		# read a file
+		li $v0, 14
+		move $a0, $t4	# curr location
+		move $a1, $t6	# input buffer
+		li $a2, 1
+		syscall
+	
+		lbu $t5, 0($t6)
+		
+		# flip MSB
+		li $t3, 0x80
+		xor $t3, $t5, $t3
+	
+		# print char---tmp
+		#move $a0, $t5
+		#li $v0, 11
+		#syscall
+		
+		# add char to map
+		sb $t3, 0($t0)
+		
+		# check if char == @
+		li $t3, '@'
+		bne $t5, $t3, init_game_loop_checked
+		sb $t9, 0($t2)	# tmp... pos = rows*cols - $t9
+		
+		init_game_loop_checked:
+		lbu $t3, 0($t1)
+		addi $t0, $t0, 1
+		addi $t9, $t9, -1
+		addi $t7, $t7, 1
+		j init_game_loop
+		
+	init_game_loop_end:
+	
+	# Calculate player position
+	lbu $t0, 0($t2)
+	mult $t3, $t8
+	mflo $t9
+	
+	sub $t0, $t9, $t0	# position
+	div $t0, $t8
+	
+	mflo $t0
+	mfhi $t9
+	sb $t0, 0($t2)
+	addi $t2, $t2, 1
+	sb $t9, 0($t2)
+	addi $t2, $t2, 1
+	
+	
+	# read a file... get STARTING_HEALTH---------------------------------
+	li $t3, 0	# $t3 = value
+	
+	li $v0, 14
+	move $a0, $t4	# curr location
+	move $a1, $t6	# input buffer
+	li $a2, 1
+	syscall
+	
+	lbu $t8, 0($t6)
+	addi $t8, $t8, -48
+	
+	li $t7, 10
+	mult $t8, $t7
+	mflo $t3
+	
+	li $v0, 14
+	move $a0, $t4	# curr location
+	move $a1, $t6	# input buffer
+	li $a2, 1
+	syscall
+	
+	lbu $t8, 0($t6)
+	addi $t8, $t8, -48
+	
+	add $t3, $t3, $t8	# HEALTH
+	
+	sb $t3, 0($t2)
+	
+	# byte 4 = 0
+	addi $t2, $t2, 1
+	li $t0, 0
+	sb $t0, 0($t2)
+	
+	# add to byte 3 of player
+	move $t2, $t3
+	
+	# close a file---------------------------------
+	li $v0, 16
+	move $a0, $t4
+	syscall
+	
+	li $v0, 0
+	
+init_game_exit:
+	jr $ra
+
+
+#####################################################################
+# Part II
+is_valid_cell:
+li $v0, -200
+li $v1, -200
+jr $ra
+
+
+#####################################################################
+# Part III
+get_cell:
+li $v0, -200
+li $v1, -200
+jr $ra
+
+
+#####################################################################
+# Part IV
+set_cell:
+li $v0, -200
+li $v1, -200
+jr $ra
+
+
+#####################################################################
+# Part V
+reveal_area:
+li $v0, -200
+li $v1, -200
+jr $ra
+
+#####################################################################
+# Part VI
+get_attack_target:
+li $v0, -200
+li $v1, -200
+jr $ra
+
+#####################################################################
+# Part VII
+complete_attack:
+li $v0, -200
+li $v1, -200
+jr $ra
+
+#####################################################################
+# Part VIII
+monster_attacks:
+li $v0, -200
+li $v1, -200
+jr $ra
+
+#####################################################################
+# Part IX
+player_move:
+li $v0, -200
+li $v1, -200
+jr $ra
+
+#####################################################################
+# Part X
+player_turn:
+li $v0, -200
+li $v1, -200
+jr $ra
+
+#####################################################################
+# Part XI
+flood_fill_reveal:
+li $v0, -200
+li $v1, -200
+jr $ra
+
+#####################################################################
+############### DO NOT CREATE A .data SECTION! ######################
+############### DO NOT CREATE A .data SECTION! ######################
+############### DO NOT CREATE A .data SECTION! ######################
+##### ANY LINES BEGINNING .data WILL BE DELETED DURING GRADING! #####
+#####################################################################
---
+++ hw4/hw4_main.asm
@@ -0,0 +1,266 @@
+.data
+map_filename: .asciiz "map3.txt"
+
+# num words for map: 45 = (num_rows * num_cols + 2) // 4 
+# map is random garbage initially
+.asciiz "Don't touch this region of memory"
+map: .word 0x632DEF01 0xAB101F01 0xABCDEF01 0x00000201 0x22222222 0xA77EF01 0x88CDEF01 0x90CDEF01 0xABCD2212 0x632DEF01 0xAB101F01 0xABCDEF01 0x00000201 0x22222222 0xA77EF01 0x88CDEF01 0x90CDEF01 0xABCD2212 0x632DEF01 0xAB101F01 0xABCDEF01 0x00000201 0x22222222 0xA77EF01 0x88CDEF01 0x90CDEF01 0xABCD2212 0x632DEF01 0xAB101F01 0xABCDEF01 0x00000201 0x22222222 0xA77EF01 0x88CDEF01 0x90CDEF01 0xABCD2212 0x632DEF01 0xAB101F01 0xABCDEF01 0x00000201 0x22222222 0xA77EF01 0x88CDEF01 0x90CDEF01 0xABCD2212 
+
+.asciiz "Don't touch this"
+# player struct is random garbage initially
+player: .word 0x2912FECD
+
+.asciiz "Don't touch this either"
+# visited[][] bit vector will always be initialized with all zeroes
+# num words for visited: 6 = (num_rows * num*cols) // 32 + 1
+visited: .word 0 0 0 0 0 0 
+.asciiz "Really, please don't mess with this string"
+
+welcome_msg: .asciiz "Welcome to MipsHack! Prepare for adventure!\n"
+pos_str: .asciiz "Pos=["
+health_str: .asciiz "] Health=["
+coins_str: .asciiz "] Coins=["
+your_move_str: .asciiz " Your Move: "
+you_won_str: .asciiz "Congratulations! You have defeated your enemies and escaped with great riches!\n"
+you_died_str: .asciiz "You died!\n"
+you_failed_str: .asciiz "You have failed in your quest!\n"
+
+.text
+j main # idk is this correct?????????????????????????????????????????????????
+
+print_map: # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ TO DO
+    la $t0, map  # the function does not need to take arguments
+        
+    
+    lbu $t1, 0($t0)
+    addi $t0, $t0, 1
+    lbu $t2, 0($t0)
+    addi $t0, $t0, 1
+    
+    move $a0, $t1
+    li $v0, 1
+    syscall
+    
+    li $a0, '\n'
+    li $v0, 11
+    syscall
+    
+    move $a0, $t2
+    li $v0, 1
+    syscall
+    
+    li $a0, '\n'
+    li $v0, 11
+    syscall
+    
+    mult $t1, $t2
+    mflo $t3
+    
+    move $t9, $t0
+    move $t8, $t3
+    li $t4, 0	# new line ctr
+    print_map_loop:
+    	bne $t4, $t2, print_map_loop_cont
+	###### new line
+	li $a0, '\n'
+    	li $v0, 11
+    	syscall
+    	li $t4, 0
+	###### new line
+	li $t7, 0	# reset
+	print_map_loop_cont:
+	beqz $t3, print_map_loop_exit
+	lbu $t5, 0($t0)
+	
+	li $t7, 0x80
+	and $t5, $t5, $t7
+	bnez $t5, print_map_char
+	lbu $t5, 0($t0)
+	
+	# print ln
+	move $a0, $t5
+    	li $v0, 11
+    	syscall
+    	
+    	addi $t4, $t4, 1
+    	addi $t0, $t0, 1
+    	addi $t3, $t3, -1
+    	j print_map_loop
+	
+	print_map_char:
+	lbu $t5, 0($t0)
+	li $t7, 0x80
+	xor $t5, $t5, $t7
+	
+	# print ln
+	move $a0, $t5
+    	li $v0, 11
+    	syscall
+    	
+    	addi $t4, $t4, 1
+    	addi $t0, $t0, 1
+    	addi $t3, $t3, -1
+    	j print_map_loop
+    print_map_loop_exit:
+    	
+    	move $t0, $t9
+    	move $t3, $t8
+    	li $t4, 0
+    print_map_two_loop:
+    	bne $t4, $t2, print_map_two_loop_cont
+	###### new line
+	li $a0, '\n'
+    	li $v0, 11
+    	syscall
+    	li $t4, 0
+	###### new line
+	li $t7, 0	# reset
+	print_map_two_loop_cont:
+	beqz $t3, print_map_loop_two_exit
+	lbu $t5, 0($t0)
+	
+	li $t9, 0x80
+	and $t5, $t5, $t9
+	
+	bnez $t5, print_map_two_space
+	lbu $t5, 0($t0)
+	
+	# print ln
+	move $a0, $t5
+    	li $v0, 11
+    	syscall
+    	
+    	addi $t4, $t4, 1
+    	addi $t0, $t0, 1
+    	addi $t3, $t3, -1
+    	j print_map_two_loop
+	
+	print_map_two_space:
+	# print ln
+	li $a0, ' '
+    	li $v0, 11
+    	syscall
+    	
+    	addi $t4, $t4, 1
+    	addi $t0, $t0, 1
+    	addi $t3, $t3, -1
+    	j print_map_two_loop
+    	
+    print_map_loop_two_exit:
+        jr $ra
+
+print_player_info: # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ TO DO
+#the idea: print something like "Pos=[3,14] Health=[4] Coins=[1]"
+	la $t0, player
+	lbu $a0, 0($t0)
+	li $v0, 1
+	syscall
+    	###### new line
+	li $a0, '\n'
+    	li $v0, 11
+    	syscall
+	###### new line
+	lbu $a0, 1($t0)
+	li $v0, 1
+	syscall
+	###### new line
+	li $a0, '\n'
+    	li $v0, 11
+    	syscall
+	###### new line
+	lbu $a0, 2($t0)
+	li $v0, 1
+	syscall
+	###### new line
+	li $a0, '\n'
+    	li $v0, 11
+    	syscall
+	###### new line
+	lbu $a0, 3($t0)
+	li $v0, 1
+	syscall
+	###### new line
+	li $a0, '\n'
+    	li $v0, 11
+    	syscall
+	###### new line
+	
+	jr $ra
+
+
+##########################################################################################
+.globl main
+main:
+la $a0, welcome_msg
+li $v0, 4
+syscall
+
+# fill in arguments
+la $a0, map_filename
+la $a1, map
+la $a2, player
+jal init_game
+
+# fill in arguments
+jal reveal_area
+
+li $s0, 0  # move = 0
+
+game_loop:  # while player is not dead and move == 0:
+
+jal print_map # takes no args
+
+jal print_player_info # takes no args
+
+# print prompt
+la $a0, your_move_str
+li $v0, 4
+syscall
+
+li $v0, 12  # read character from keyboard
+syscall
+move $s1, $v0  # $s1 has character entered
+li $s0, 0  # move = 0
+
+li $a0, '\n'
+li $v0 11
+syscall
+
+# handle input: w, a, s or d
+# map w, a, s, d  to  U, L, D, R and call player_turn()
+
+# if move == 0, call reveal_area()  Otherwise, exit the loop.
+
+j game_loop
+
+game_over:
+#jal print_map
+#jal print_player_info
+li $a0, '\n'
+li $v0, 11
+syscall
+
+# choose between (1) player dead, (2) player escaped but lost, (3) player escaped and won
+
+won:
+la $a0, you_won_str
+li $v0, 4
+syscall
+j exit
+
+failed:
+la $a0, you_failed_str
+li $v0, 4
+syscall
+j exit
+
+player_dead:
+la $a0, you_died_str
+li $v0, 4
+syscall
+
+exit:
+li $v0, 10
+syscall
+
+.include "hw4.asm"
